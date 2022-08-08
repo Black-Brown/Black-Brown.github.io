7d7ff9f2f9700aff4345544dc30d0ebfb6afeb18
Black-Brown <jheinelbrown@gmail.com>
2022-08-08 18:34:24 -0400

    Abrazo-Virtual

---
+++ js/main.js
@@ -0,0 +1,9 @@
+setTimeout(cambiaMensaje, 2300)
+
+function cambiaMensaje(){
+document.getElementById("loading-cargando").innerText = "Enviando!"
+}
+
+setTimeout(function(){
+    alert("Recibiste un Abrazo!!! Espero que te guste el abrazo, by Jheinel Brown ")
+}, 2400)
